1aad5c161684c20f1ddfc23048d5504a6d1383c3
jeanq1 <pers.jean@gmail.com>
2022-01-30 11:37:50 +0100

    added all modelsand changed trainer and dataloaders

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -10,7 +10,7 @@ class Trainer():
     Trainer class
     """
     def __init__(self, model, criterion, optimizer, data_loader, device,
-                 valid_data_loader=None, class_weights=None):
+                 valid_data_loader=None, class_weights=None, requires_softmax = False):
 
         self.model = model
         self.criterion = criterion
@@ -21,7 +21,7 @@ class Trainer():
         self.optimizer = optimizer
         self.class_weights = torch.tensor(class_weights, dtype=torch.double)
         self.device = device
-
+        self.requires_softmax = requires_softmax
 
 
 
@@ -33,7 +33,13 @@ class Trainer():
         losses = []
 
         for (features, target) in tqdm(self.data_loader):
+            
+            
+            
+            self.model.train()
+            
             self.optimizer.zero_grad()
+            
             predictions = self.model(features)
             
             loss = self.criterion(predictions, target)
@@ -42,21 +48,28 @@ class Trainer():
 
             loss.backward()
             self.optimizer.step()
+            
             batch_id+=1
 
             
-
+        self.model.eval()
         if self.do_validation:
             with torch.no_grad():
                 val_correct_preds = 0
                 val_losses = []
+                total = 0
                 for (features, target) in (self.valid_data_loader):
                     predictions = self.model(features)
+                    #import pdb; pdb.set_trace()
+                    if self.requires_softmax:
+                        print("hello requires softmax")
+                        predictions = nn.functional.log_softmax(predictions, dim=1)
                     predicted_classes = torch.argmax(predictions, dim=1)
                     val_correct_preds += int(torch.sum(predicted_classes==target))
+                    total += predictions.shape[0]
                     val_losses.append(float(self.criterion(predictions, target)))
                 
-                accuracy = val_correct_preds/len(self.valid_data_loader)
+                accuracy = val_correct_preds/total
                 loss = np.mean(val_losses)
                 print('average train loss: ', np.mean(losses))
                 print('validation loss: ', loss)
